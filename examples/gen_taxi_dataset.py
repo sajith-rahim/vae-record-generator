9b5697d7bdf0d4ed2e63db5cd19277e758a79df8
sajith.rahim <sajith.rahim@outlook.com>
2022-01-21 00:57:11 +0530

    Update gen_taxi_dataset.py

--- examples/gen_taxi_dataset.py
+++ examples/gen_taxi_dataset.py
@@ -1,5 +1,12 @@
 def run():
-    pass
+    data, discrete_columns = read_csv('datasets/taxi.csv', True, '0,1,2,5,6,7')
+    print('***')
+
+    model = VAE()
+    model.fit(data, tuple(discrete_columns))
+
+    sampled = model.sample(10)
+    print(sampled)
 
 if __name__ == '__main__':
     run()
