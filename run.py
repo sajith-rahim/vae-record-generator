0891951ed6272a69e090fd9baa9055bbf8a6ed5a
sajith.rahim <sajith.rahim@outlook.com>
2022-01-21 00:57:02 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from utils.file_utils import read_csv
 
 def run():
     data, discrete_columns = read_csv('datasets/taxi.csv', True, '0,1,2,5,6,7')
-    # data = data.iloc[1:500, :]
+    data = data.iloc[1:100, :]
     print('***')
 
     model = VAE()
